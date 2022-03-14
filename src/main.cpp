1659dddcc073183e5939b3e6b29e5ca29eadfb28
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-14 12:46:48 +0100

    Base LargeNumber and ExactNumber class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+
+class LargeNumber {
+  bool positive;
+  std::vector<unsigned long> number;
+
+public:
+  LargeNumber(long num) : number() {
+    positive = (num >= 0);
+    number.push_back(std::abs(num));
+  }
+};
+
+class ExactNumber {
+  LargeNumber numerator;   // citatel
+  LargeNumber denominator; // jmenovatel
+  LargeNumber exponent;
+
+public:
+  ExactNumber(long numerator, long denominator = 1, long exponent = 0)
+      : numerator(numerator), denominator(denominator), exponent(exponent) {}
+};
 
 int main() { return 0; }
